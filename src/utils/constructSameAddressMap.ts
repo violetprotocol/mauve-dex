483649846311bbb855b4fa0e3efc59e76005fc98
ra-phael <10075759+ra-phael@users.noreply.github.com>
2023-01-12 14:20:08 +0100

    makes address maps overridable

--- src/utils/constructSameAddressMap.ts
+++ src/utils/constructSameAddressMap.ts
@@ -10,10 +10,17 @@ const DEFAULT_NETWORKS = [
 
 export function constructSameAddressMap<T extends string>(
   address: T,
-  additionalNetworks: SupportedChainId[] = []
+  additionalNetworks: SupportedChainId[] = [],
+  override?: Partial<Record<SupportedChainId, string>>
 ): { [chainId: number]: T } {
-  return DEFAULT_NETWORKS.concat(additionalNetworks).reduce<{ [chainId: number]: T }>((memo, chainId) => {
+  const addressMap = DEFAULT_NETWORKS.concat(additionalNetworks).reduce<{ [chainId: number]: T }>((memo, chainId) => {
+    if (override && chainId in override) {
+      memo[chainId] = override[chainId] as T
+      return memo
+    }
     memo[chainId] = address
     return memo
   }, {})
+
+  return addressMap
 }
