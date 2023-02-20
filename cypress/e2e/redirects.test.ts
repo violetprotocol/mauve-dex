627e2d8b85e3b845713a6fa7ace7f94109f365a8
ra-phael <10075759+ra-phael@users.noreply.github.com>
2023-02-20 16:13:36 +0100

    remove / skip some e2e tests

--- cypress/e2e/redirects.test.ts
+++ cypress/e2e/redirects.test.ts
@@ -1,5 +1,5 @@
 describe('Redirect', () => {
-  it('should redirect to /vote/create-proposal when visiting /create-proposal', () => {
+  it.skip('should redirect to /vote/create-proposal when visiting /create-proposal', () => {
     cy.visit('/create-proposal')
     cy.url().should('match', /\/vote\/create-proposal/)
   })
--- cypress/e2e/remove-liquidity.test.ts
+++ cypress/e2e/remove-liquidity.test.ts
@@ -1,4 +1,5 @@
-describe('Remove Liquidity', () => {
+// TODO-MAUVE: Update with V3
+describe.skip('Remove Liquidity', () => {
   it('eth remove', () => {
     cy.visit('/remove/v2/ETH/0xF9bA5210F91D0474bd1e1DcDAeC4C58E359AaD85')
     cy.get('#remove-liquidity-tokena-symbol').should('contain.text', 'ETH')
