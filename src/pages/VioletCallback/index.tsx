86ffb3998941f0d5055c8dd1fa63e301af9f43ad
Pedro Filho <pedro@filho.me>
2023-04-21 15:56:17 +0100

    adding VIOLET_AUTHORIZATION_JSON and using correct channel

--- src/pages/VioletCallback/index.tsx
+++ src/pages/VioletCallback/index.tsx
@@ -1,25 +1,21 @@
-import { VIOLET_AUTHORIZATION_JSON } from '@violetprotocol/sdk'
+import { VIOLET_AUTHORIZATION_CHANNEL } from '@violetprotocol/sdk'
 import { useEffect } from 'react'
 
 const VioletCallback = () => {
   useEffect(() => {
     const searchParams = new URLSearchParams(window.location.search)
 
-    const params: Record<string, string | number> = {}
+    const params: { [key: string]: string } = {}
 
     for (const [key, value] of searchParams.entries()) {
       params[key] = value
     }
 
-    if (params['tx_id']) {
-      localStorage.setItem(VIOLET_AUTHORIZATION_JSON, JSON.stringify(params))
+    const channel = new BroadcastChannel(VIOLET_AUTHORIZATION_CHANNEL)
 
-      window.close()
+    channel.postMessage(params)
 
-      return
-    }
-
-    window.location.href = '/#/swap'
+    window.close()
   }, [])
 
   return null
