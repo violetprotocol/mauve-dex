7d064dcd15b0e2ca0500ab8a35bd964511d4e30d
ra-phael <10075759+ra-phael@users.noreply.github.com>
2023-06-02 12:36:21 +0100

    restore some cypress tests and update them to OP Goerli

--- cypress/e2e/add-liquidity.test.ts
+++ cypress/e2e/add-liquidity.test.ts
@@ -10,30 +10,31 @@ describe('Add Liquidity', () => {
   })
 
   it('loads the two correct tokens', () => {
-    cy.visit('/add/0xF9bA5210F91D0474bd1e1DcDAeC4C58E359AaD85/0xc778417E063141139Fce010982780140Aa0cD5Ab/500')
-    cy.get('#add-liquidity-input-tokena .token-symbol-container').should('contain.text', 'MKR')
+    cy.visit('/add/0x32307adfFE088e383AFAa721b06436aDaBA47DBE/0x4200000000000000000000000000000000000006/500')
+    cy.get('#add-liquidity-input-tokena .token-symbol-container').should('contain.text', 'OUT-1')
     cy.get('#add-liquidity-input-tokenb .token-symbol-container').should('contain.text', 'ETH')
   })
 
   it('does not crash if ETH is duplicated', () => {
-    cy.visit('/add/0xc778417E063141139Fce010982780140Aa0cD5Ab/0xc778417E063141139Fce010982780140Aa0cD5Ab')
+    cy.visit('/add/eth/eth')
     cy.get('#add-liquidity-input-tokena .token-symbol-container').should('contain.text', 'ETH')
     cy.get('#add-liquidity-input-tokenb .token-symbol-container').should('not.contain.text', 'ETH')
   })
 
-  it.skip('token not in storage is loaded', () => {
-    cy.visit('/add/0xb290b2f9f8f108d03ff2af3ac5c8de6de31cdf6d/0xF9bA5210F91D0474bd1e1DcDAeC4C58E359AaD85')
-    cy.get('#add-liquidity-input-tokena .token-symbol-container').should('contain.text', 'SKL')
-    cy.get('#add-liquidity-input-tokenb .token-symbol-container').should('contain.text', 'MKR')
+  it('token not in storage is loaded', () => {
+    cy.visit('/add/0x1888649D566908E0A4Ac17978740F6A04f600a51/0xb378eD8647D67b5dB6fD41817fd7a0949627D87a')
+    cy.get('#add-liquidity-input-tokena .token-symbol-container').should('contain.text', 'tUSDC')
+    cy.get('#add-liquidity-input-tokenb .token-symbol-container').should('contain.text', 'OUT-2')
   })
 
-  it.skip('single token can be selected', () => {
-    cy.visit('/add/0xb290b2f9f8f108d03ff2af3ac5c8de6de31cdf6d')
-    cy.get('#add-liquidity-input-tokena .token-symbol-container').should('contain.text', 'SKL')
-    cy.visit('/add/0xF9bA5210F91D0474bd1e1DcDAeC4C58E359AaD85')
-    cy.get('#add-liquidity-input-tokena .token-symbol-container').should('contain.text', 'MKR')
+  it('single token can be selected', () => {
+    cy.visit('/add/0x1888649D566908E0A4Ac17978740F6A04f600a51')
+    cy.get('#add-liquidity-input-tokena .token-symbol-container').should('contain.text', 'tUSDC')
+    cy.visit('/add/0x32307adfFE088e383AFAa721b06436aDaBA47DBE')
+    cy.get('#add-liquidity-input-tokena .token-symbol-container').should('contain.text', 'OUT-1')
   })
 
+  // TODO: Un-skip when subgraph is done
   it.skip('loads fee tier distribution', () => {
     cy.fixture('feeTierDistribution.json').then((feeTierDistribution) => {
       cy.intercept('POST', '/subgraphs/name/uniswap/uniswap-v3', (req: CyHttpMessages.IncomingHttpRequest) => {
--- cypress/e2e/token.test.ts
+++ cypress/e2e/token.test.ts
@@ -18,7 +18,8 @@ describe('Testing tokens on uniswap page', () => {
     cy.get(getTestSelector('tokens-network-filter-selected')).should('contain', 'Optimism')
   })
 
-  it('should load go to ethereum token and return to token list page', () => {
+  // [MAUVE-DISABLED] This page is out of scope for now
+  it.skip('should load go to ethereum token and return to token list page', () => {
     cy.visit('/tokens/ethereum')
     cy.get(getTestSelector('token-table-row-Ether')).click()
     cy.get(getTestSelector('token-details-stats')).should('exist')
@@ -26,14 +27,16 @@ describe('Testing tokens on uniswap page', () => {
     cy.get(getTestSelectorStartsWith('token-table')).its('length').should('be.gte', 25)
   })
 
-  it('should go to native token on ethereum and render description', () => {
+  // [MAUVE-DISABLED] This page is out of scope for now
+  it.skip('should go to native token on ethereum and render description', () => {
     cy.visit('/tokens/ethereum/NATIVE')
     cy.get(getTestSelector('token-details-about-section')).should('exist')
     cy.contains('Ethereum is a smart contract platform that enables developers').should('exist')
     cy.contains('Etherscan').should('exist')
   })
 
-  it('should go to native token on polygon and render description and links', () => {
+  // [MAUVE-DISABLED] This page is out of scope for now
+  it.skip('should go to native token on polygon and render description and links', () => {
     cy.visit('/tokens/polygon/NATIVE')
     cy.get(getTestSelector('token-details-about-section')).should('exist')
     cy.contains('Wrapped Matic on Polygon').should('exist')
