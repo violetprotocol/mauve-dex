0e1f48cfe6bff525be272727cca2cedcc9bf0f81
Gabriel Ferraz <sudoferraz@gmail.com>
2023-07-03 13:20:04 +0300

    Adds newRelic script directly on script tag

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { Provider } from 'react-redux'
 import { HashRouter } from 'react-router-dom'
-import { isDevelopmentEnv, isSentryEnabled } from 'utils/env'
+import { isSentryEnabled } from 'utils/env'
 
 import Web3Provider from './components/Web3Provider'
 import { LanguageProvider } from './i18n'
@@ -53,12 +53,12 @@ const queryClient = new QueryClient()
 
 const container = document.getElementById('root') as HTMLElement
 
-const newRelicDevAgent = <script async type="text/javascript" src="/public/mauveNewRelicDev.js" />
-const newRelicProdAgent = <script async type="text/javascript" src="/public/mauveNewRelicProd.js" />
+// const newRelicDevAgent = <script async type="text/javascript" src="/public/mauveNewRelicDev.js" />
+// const newRelicProdAgent = <script async type="text/javascript" src="/public/mauveNewRelicProd.js" />
 
 createRoot(container).render(
   <StrictMode>
-    {isDevelopmentEnv() ? newRelicDevAgent : newRelicProdAgent}
+    <script async type="text/javascript" src="/public/mauveNewRelicDev.js" />
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
         <HashRouter>
