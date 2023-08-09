5ffc22c1f7c1a9a19bf837f6a617ccbf405d00a4
Papa Smurf <chris@violet.co>
2023-08-09 15:02:47 +0100

    chore: lint

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -4,7 +4,6 @@ const MAUVE_TESTNETS_LIST_URL =
   'https://raw.githubusercontent.com/violetprotocol/mauve-token-list/main/mauve.tokenlist.testnets.json'
 const MAUVE_MAINNETS_LIST_URL =
   'https://raw.githubusercontent.com/violetprotocol/mauve-token-list/main/mauve.tokenlist.mainnets.json'
-const UNI_LIST = 'https://tokens.uniswap.org'
 const UNI_EXTENDED_LIST = 'https://extendedtokens.uniswap.org/'
 const UNI_UNSUPPORTED_LIST = 'https://unsupportedtokens.uniswap.org/'
 const AAVE_LIST = 'tokenlist.aave.eth'
