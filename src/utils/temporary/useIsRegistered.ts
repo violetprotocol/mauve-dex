b20248e6ecb1ba174bdac98578285e5af4045051
ra-phael <10075759+ra-phael@users.noreply.github.com>
2023-09-05 23:06:43 +0100

    add wip utils

---
+++ src/utils/temporary/useIsRegistered.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react'
+
+// Temporary switch to simulate different responses from Violet
+const IS_ALREADY_ENROLLED = false
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
+// TODO: Sth like that should be provided by Violet SDK
+export const useIsRegisteredWithViolet = ({ ethereumAddress }: { ethereumAddress?: string }) => {
+  const [isRegistered, setIsRegistered] = useState<null | boolean>(null)
+
+  const fetchStatus = async (_ethereumAddress: string) => {
+    await sleep(1000)
+
+    return IS_ALREADY_ENROLLED
+  }
+
+  useEffect(() => {
+    if (ethereumAddress) {
+      fetchStatus(ethereumAddress).then((value) => setIsRegistered(value))
+    }
+  }, [ethereumAddress])
+
+  return { isRegistered }
+}
