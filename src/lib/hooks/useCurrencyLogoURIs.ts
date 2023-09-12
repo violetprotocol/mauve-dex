275d74086ad18ec78fab41a75979fd4726577c02
Papa Smurf <chris@violet.co>
2023-09-12 17:55:45 +0200

    chore: lint

--- src/lib/hooks/useCurrencyLogoURIs.ts
+++ src/lib/hooks/useCurrencyLogoURIs.ts
@@ -1,12 +1,8 @@
 import { SupportedChainId } from 'constants/chains'
-import useHttpLocations from 'hooks/useHttpLocations'
-import { useMemo } from 'react'
-import { isAddress } from 'utils'
 
 import EthereumLogo from '../../assets/images/ethereum-logo.png'
 import CeloLogo from '../../assets/svg/celo_logo.svg'
 import MaticLogo from '../../assets/svg/matic-token-icon.svg'
-import { isCelo, NATIVE_CHAIN_ID, nativeOnChain } from '../../constants/tokens'
 
 type Network = 'ethereum' | 'arbitrum' | 'optimism' | 'polygon'
 
@@ -36,49 +32,4 @@ export function getNativeLogoURI(chainId: SupportedChainId = SupportedChainId.MA
     default:
       return EthereumLogo
   }
-}
-
-function getTokenLogoURI(address: string, chainId: SupportedChainId = SupportedChainId.MAINNET): string | void {
-  const networkName = chainIdToNetworkName(chainId)
-  const networksWithUrls = [SupportedChainId.ARBITRUM_ONE, SupportedChainId.MAINNET, SupportedChainId.OPTIMISM]
-  if (networksWithUrls.includes(chainId)) {
-    return `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/${networkName}/assets/${address}/logo.png`
-  }
-
-  // Celo logo logo is hosted elsewhere.
-  if (isCelo(chainId)) {
-    if (address === nativeOnChain(chainId).wrapped.address) {
-      return 'https://raw.githubusercontent.com/ubeswap/default-token-list/master/assets/asset_CELO.png'
-    }
-  }
-}
-
-export default function useCurrencyLogoURIs(
-  currency:
-    | {
-        isNative?: boolean
-        isToken?: boolean
-        address?: string
-        chainId: number
-        logoURI?: string | null
-      }
-    | null
-    | undefined
-): string[] {
-  const locations = useHttpLocations(currency?.logoURI)
-  return useMemo(() => {
-    const logoURIs = [...locations]
-    if (currency) {
-      if (currency.isNative || currency.address === NATIVE_CHAIN_ID) {
-        logoURIs.push(getNativeLogoURI(currency.chainId))
-      } else if (currency.isToken || currency.address) {
-        const checksummedAddress = isAddress(currency.address)
-        const logoURI = checksummedAddress && getTokenLogoURI(checksummedAddress, currency.chainId)
-        if (logoURI) {
-          logoURIs.push(logoURI)
-        }
-      }
-    }
-    return logoURIs
-  }, [currency, locations])
 }
