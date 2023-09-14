82f85c22111c6dd16de7d01f9e9e9f5bccba6428
Pedro Filho <pedro@filho.me>
2023-09-14 10:44:47 +0200

    feat: violet header docs (#147)

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -806,6 +806,7 @@ export default function Swap({ className }: { className?: string }) {
               </div>
             </AutoColumn>
           </SwapWrapper>
+
           <VioletCTA />
         </PageWrapper>
         <SwitchLocaleLink />
