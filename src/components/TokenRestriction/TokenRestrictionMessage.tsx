0ed0516a9ccb508e651f2f1e57dc975f6859b15c
Papa Smurf <chris@violet.co>
2023-09-27 16:15:47 +0100

    feat: adds a modal that is shown when selecting a restricted token

---
+++ src/components/TokenRestriction/TokenRestrictionMessage.tsx
@@ -0,0 +1,83 @@
+import { AutoColumn } from 'components/Column'
+import { getRestrictionCopy, TOKEN_RESTRICTION_TYPE } from 'constants/tokenRestrictions'
+import { getWarningCopy, TOKEN_SAFETY_ARTICLE, Warning, WARNING_LEVEL } from 'constants/tokenSafety'
+import { useTokenWarningColor, useTokenWarningTextColor } from 'hooks/useTokenWarningColor'
+import { AlertTriangle, Slash } from 'react-feather'
+import { Text } from 'rebass'
+import styled from 'styled-components/macro'
+import { ExternalLink } from 'theme'
+
+const Label = styled.div<{ color: string; backgroundColor: string }>`
+  width: 100%;
+  padding: 20px 20px;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  border-radius: 16px;
+  color: ${({ color }) => color};
+`
+
+const TitleRow = styled.div`
+  align-items: center;
+  font-weight: 700;
+  display: inline-flex;
+`
+
+const Title = styled(Text)`
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 24px;
+  margin-left: 7px;
+`
+
+const DetailsRow = styled.div`
+  margin: 10px 10px;
+  font-size: 12px;
+  line-height: 16px;
+  color: ${({ theme }) => theme.textSecondary};
+`
+
+// const StyledLink = styled(ExternalLink)`
+//   color: ${({ theme }) => theme.textSecondary};
+//   font-weight: 700;
+// `
+
+const StyledLink = styled(ExternalLink)`
+  background-color: transparent;
+  border: 1px solid ${({ theme }) => theme.backgroundContrast};
+  border-radius: ${20}px;
+  color: ${({ theme }) => theme.textPrimary};
+  cursor: pointer;
+  font-weight: 600;
+  font-size: 13px;
+  padding: 10px 16px;
+  text-align: center;
+`
+
+type TokenRestrictionMessageProps = {
+  restriction: TOKEN_RESTRICTION_TYPE
+}
+
+export default function TokenRestrictionMessage({ restriction }: TokenRestrictionMessageProps) {
+  const backgroundColor = useTokenWarningColor(WARNING_LEVEL.MEDIUM)
+  const textColor = useTokenWarningTextColor(WARNING_LEVEL.MEDIUM)
+  const { heading, description, action, link } = getRestrictionCopy(restriction)
+
+  return (
+    <Label color={textColor} backgroundColor={backgroundColor}>
+      <TitleRow>
+        <AlertTriangle size="16px" />
+        <Title marginLeft="7px">{heading} </Title>
+      </TitleRow>
+
+      <DetailsRow>
+        {Boolean(heading) && ' '}
+        {description}
+        {Boolean(description) && ' '}
+      </DetailsRow>
+      <AutoColumn>
+        <StyledLink href={link ?? ""}>
+          <>{action}</>
+        </StyledLink>
+      </AutoColumn>
+    </Label>
+  )
+}
