b52940383cf8c6b68589c399c9ce94d48c3d3a0c
Papa Smurf <chris@violet.co>
2023-10-11 17:10:11 +0100

    feat: swaps pass token exclusions when fetching best trade

--- src/hooks/useV3SwapPools.ts
+++ src/hooks/useV3SwapPools.ts
@@ -4,6 +4,7 @@ import { useWeb3React } from '@web3-react/core'
 import { SupportedChainId } from 'constants/chains'
 import { useMemo } from 'react'
 
+import { useTokensToExclude } from './Tokens'
 import { useAllCurrencyCombinations } from './useAllCurrencyCombinations'
 import { PoolState, usePools } from './usePools'
 
@@ -20,8 +21,8 @@ export function useV3SwapPools(
   loading: boolean
 } {
   const { chainId } = useWeb3React()
-
-  const allCurrencyCombinations = useAllCurrencyCombinations(currencyIn, currencyOut)
+  const excludeTokens = useTokensToExclude()
+  const allCurrencyCombinations = useAllCurrencyCombinations(currencyIn, currencyOut, excludeTokens)
 
   const allCurrencyCombinationsWithAllFees: [Token, Token, FeeAmount][] = useMemo(
     () =>
