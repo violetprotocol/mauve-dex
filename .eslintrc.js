26eb92378bac70cd155378d2fb9596665ca8b825
Gabriel Ferraz <sudoferraz@gmail.com>
2023-11-16 12:47:38 +0200

    Adds ignore rules for prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,19 @@ module.exports = {
   extends: '@uniswap/eslint-config/react',
   overrides: [
     {
-      files: ['src/constants/chains.ts', 'src/constants/locales.ts', 'src/state/routing/utils.ts'],
+      files: [
+        'src/constants/chains.ts',
+        'src/constants/locales.ts',
+        'src/state/routing/utils.ts',
+        'src/utils/violet/authorizeProps.ts',
+        'src/utils/unwrappedToken.ts',
+        'src/utils/prices.ts',
+        'src/components/AccountDetails/Transaction.tsx',
+        'src/components/AccountDetails/TransactionSummary.tsx',
+        'src/components/AccountDetails/index.tsx',
+        'src/components/AccountDetailsV2/LogoView.tsx',
+        'src/components/AccountDetailsV2/TransactionBody.tsx',
+      ],
       rules: {
         'prettier/prettier': 'off',
       },
